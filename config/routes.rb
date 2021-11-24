7b6cb92427e7c7f12204dc2edbf79a8d64d2d210
Gustavo Araujo <garaujodev@gmail.com>
2021-11-24 14:00:45 -0300

    Add `/users/registration` route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :users do
+    resources :registrations, only: :create
+  end
 end
