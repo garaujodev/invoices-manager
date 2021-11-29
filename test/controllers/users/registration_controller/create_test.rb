0e73748eb27e65d0a4f3b637c34021e1145248b1
Gustavo Araujo <garaujodev@gmail.com>
2021-11-28 23:08:08 -0300

    Use single quotes in registration create test flow

--- test/controllers/users/registration_controller/create_test.rb
+++ test/controllers/users/registration_controller/create_test.rb
@@ -1,44 +1,44 @@
 require 'test_helper'
 
 class Users::RegistrationsControllerCreateTest < ActionDispatch::IntegrationTest
-  test "should respond with 400 when the user param is missing" do
+  test 'should respond with 400 when the user param is missing' do
     post users_registrations_url
 
     assert_response 400
 
     assert_equal(
-      { "error" => "param is missing or the value is empty: user" },
+      { 'error' => 'param is missing or the value is empty: user' },
       JSON.parse(response.body)
     )
   end
 
-  test "should respond with 400 when the user email params are missing" do
+  test 'should respond with 400 when the user email params are missing' do
     post users_registrations_url, params: { user: {} }
 
     assert_response 400
 
     assert_equal(
-      { "error" => "param is missing or the value is empty: user" },
+      { 'error' => 'param is missing or the value is empty: user' },
       JSON.parse(response.body)
     )
   end
 
-  test "should respond with 422 when the user email is invalid" do
+  test 'should respond with 422 when the user email is invalid' do
     post users_registrations_url, params: { user: { email: 'example'} }
 
     assert_response 422
 
     assert_equal(
       {
-        "user" => {
-          "email" => ["is invalid"]
+        'user' => {
+          'email' => ['is invalid']
         }
       },
       JSON.parse(response.body)
     )
   end
 
-  test "should respond with 201 when creating the user" do
+  test 'should respond with 201 when creating the user' do
     assert_difference 'User.count', +1 do
       post users_registrations_url, params: { user: { email: 'example@email.com' } }
     end
@@ -48,7 +48,6 @@ class Users::RegistrationsControllerCreateTest < ActionDispatch::IntegrationTest
     response_data = JSON.parse(response.body)
     user = response_data['user']
 
-    assert_equal("example@email.com", user["email"])
-    assert_kind_of(Numeric, user["id"])
+    assert_equal 'example@email.com', user['email']
   end
 end
